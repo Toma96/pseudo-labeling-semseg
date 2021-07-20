22c9bfe9b3839e8bc2791b280296b5c7bedc160c
Toma <60894511+Toma96@users.noreply.github.com>
2021-07-20 21:28:54 +0200

    Add files via upload

---
+++ models/resnet_cifar.py
@@ -0,0 +1,237 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.util import SpatialPyramidPooling, _BNReluConv
+try:
+  from torch.hub import load_state_dict_from_url
+except ImportError:
+  from torch.utils.model_zoo import load_url as load_state_dict_from_url
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'resnext50_32x4d': 'https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth',
+    'resnext101_32x8d': 'https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth',
+    'wide_resnet50_2': 'https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth',
+    'wide_resnet101_2': 'https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth',
+}
+
+upsample = lambda x, size: F.interpolate(x, size, mode='bilinear', align_corners=True)
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=12, sem_seg=True, num_features=128, zero_init_residual=False,
+                 mean=(111.376, 63.110, 83.670), std=(41.608, 54.237, 68.889),
+                 spp_grids=(8, 4, 2, 1), spp_square_grid=False, spp_drop_rate=0.0,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None, target_size=None, **kwargs):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self.sem_seg = sem_seg
+        self._norm_layer = norm_layer
+        self.num_classes = num_classes
+        self.inplanes = 64
+        self.dilation = 1
+        self.register_buffer('img_mean', torch.tensor(mean).view(1, -1, 1, 1))
+        self.register_buffer('img_std', torch.tensor(std).view(1, -1, 1, 1))
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(
+                replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.target_size = target_size
+        if self.target_size is not None:
+            h, w = target_size
+            target_sizes = [(h // 2 ** i, w // 2 ** i) for i in range(2, 6)]
+        else:
+            target_sizes = [None] * 4
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2,
+                               padding=3, bias=False) if sem_seg else nn.Conv2d(3, self.inplanes, kernel_size=3,
+                                                                                stride=1, padding=1, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.fc = nn.Linear(512, num_classes)
+
+        num_levels = 3
+        self.spp_size = kwargs.get('spp_size', num_features)
+        bt_size = self.spp_size
+
+        level_size = self.spp_size // num_levels
+
+        self.spp = SpatialPyramidPooling(self.inplanes, num_levels, bt_size=bt_size, level_size=level_size,
+                                         out_size=num_features, grids=spp_grids, square_grid=spp_square_grid,
+                                         bn_momentum=0.01 / 2, use_bn=True, drop_rate=spp_drop_rate
+                                         , fixed_size=target_sizes[3])
+
+        self.logits = _BNReluConv(num_features, self.num_classes, k=3, bias=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        # pdb.set_trace()
+        if self.sem_seg:
+            # x -= self.img_mean
+            # x /= self.img_std
+            B, C, H, W = x.shape
+
+        x0 = self.conv1(x)
+
+        if self.sem_seg:
+            x0 = self.bn1(x0)
+            x0 = self.relu(x0)
+            x0 = self.maxpool(x0)  # izlaz   [64, 128, 128]
+
+        x1 = self.layer1(x0)  # izlaz    [64, 128, 128]
+        x2 = self.layer2(x1)  # izlaz    [128, 64, 64]
+        x3 = self.layer3(x2)  # izlaz   [256, 32, 32]
+        x4 = self.layer4(x3)  # izlaz    [512, 16, 16]
+
+        if self.sem_seg:
+            x5 = self.spp.forward(x4)  # izlaz  [128, 16, 16]
+            x6 = self.logits(x5)       # izlaz [n_klasa, 16, 16]
+
+            logits = torch.nn.functional.interpolate(x6, size=(H, W), mode='bilinear')
+
+        else:
+            x5 = self.avgpool(x4)
+            B, C, H, W = x5.shape
+            logits = self.fc(x5.view(B, -1))
+
+        return logits
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+def _resnet(arch, block, layers, pretrained, progress, n_classes, **kwargs):
+    model = ResNet(block, layers, num_classes=n_classes, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict, strict=False)
+
+    return model
+
+
+def ModifResNet18(pretrained=False, n_classes=12, progress=True, **kwargs):
+    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained=pretrained, progress=progress, n_classes=n_classes,
+                   **kwargs)
+
+
+if __name__ == '__main__':
+    i = torch.Tensor(1, 3, 512, 512).cuda()
+    m = ModifResNet18(pretrained=True, n_classes=10).cuda()
+    m.eval()
+    o = m(i)
+    print(o.shape)
+    print(o[0].size())
+    print("output length: ", len(o))
